3c08e887c695b10c368c616ced793f4186d2391e
78109732 <ankanr596@gmail.com>
2025-10-17 20:39:50 +0530

    Hello

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import fs from "fs";
 
 dotenv.config({});
 const app = express();
@@ -32,6 +33,14 @@ app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
+
+// if(process.env.NODE_ENV==="production"){
+//   const dirpath=Path.resolve();
+//   app.use(express.static('./Frontend/dist'))
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(dirpath,'./Frontend/dist','index.html'))
+//   })
+// }
 
 app.listen(PORT, () => {
   connectDB();

d2262ad556d014581933edf0e89b6991b1610945
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-11-01 21:11:11 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -138,7 +138,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "None",
+        sameSite: "none",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
